bce5db19c08c3f2f4cf49f7cc6adb70764a10948
@sebachien <sebastien.louis.69@gmail.com>
2021-03-17 19:33:51 +0100

    Add -add API, get result API and event-

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const addButtonNewBook = () => {
     button.setAttribute("id","addBook");
     console.log(selectButtonPosition);
     selectButtonPosition.after(button);
+    newBookButtonClick();
 }
 
 const removeButtonNewBook = () => {
@@ -18,6 +19,8 @@ const addNewFormSearch = () => {
     form.setAttribute("id","form");
     form.innerHTML ='<div class="formAddBook"><label for="title">Titre du livre </label><br/><input type="text" name="title" id="title" required></div><div class="formAddBook"><label for="author">Auteur </label><br/><input type="text" name="author" id="author" required></div><div class="formAddBook"><input type="submit" value="Rechercher" id ="search"></div><div class="formAddBook"><input type="submit" value="Annuler" id ="cancel"></div>';
     selectFormPosition.after(form);
+    cancelButtonCLick();
+    searchButtonClick();
 }
 
 const removeNewFormSearch = () => {
@@ -25,6 +28,78 @@ const removeNewFormSearch = () => {
     selectFormPosition.remove();
 }
 
+const cancelButtonCLick = () => {
+    var cancelButton = document.getElementById("cancel");
+    cancelButton.addEventListener('click', function() {
+        removeNewFormSearch();
+        addButtonNewBook();               
+    });
+}
+
+const newBookButtonClick = () => {
+    var newBookButton = document.getElementById("addBook");
+    newBookButton.addEventListener('click', function() {
+        removeButtonNewBook();
+        addNewFormSearch();               
+    });
+}
+
+const searchButtonClick = () => {
+    var searchButton = document.getElementById("search");
+    searchButton.addEventListener('click', function(e) {
+        e.preventDefault(); 
+        ApiCallback();                  
+    });
+}
+
+const ApiCallback = () => {
+    var title = document.getElementById("title");
+    var author = document.getElementById("author");
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            var response = JSON.parse(this.responseText);
+            console.log(response);
+            searchResult(response);
+        }
+    };
+    request.open("GET", "https://www.googleapis.com/books/v1/volumes?q=" + title.value + "+inauthor:" + author.value);
+    request.send();
+}
+
+const searchResult = (response) => {
+    let Books = [];
+    let image = "introuvable";
+    for (let i of response.items) {
+        if (i.volumeInfo.imageLinks != null) {
+            image = i.volumeInfo.imageLinks;
+        }
+        console.log(image);
+        Books.push(new Book(i.id, i.volumeInfo.title, i.volumeInfo.authors[0], i.volumeInfo.imageLinks[1], i.volumeInfo.description));
+        
+    }
+    console.log(Books);
+    return Books;
+
+}
+
+class Book  {
+    constructor(id,title,author,image,description) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.image = image;
+        this.description = description;
+    }
+}
 
 addButtonNewBook();
-addNewFormSearch();
+
+
+//window.localStorage.getItem('item', 123)
+
+// author : .items[].volumeInfo.authors[0]
+// title : .items[].volumeInfo.title
+// id :  .items[].accessInfo.id
+//image : items[].volumeInfo.imageLinks.thumbmail
+// description : items[].volumeInfo.description   (200 caractère max)
