03e23071a770c419813274c3218d09e21ce67b2e
@sebachien <sebastien.louis.69@gmail.com>
2021-03-26 19:30:02 +0100

    Add -add sessionStorage and fav display (v1)-

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ const addButtonNewBook = () => {
     console.log(selectButtonPosition);
     selectButtonPosition.after(button);
     newBookButtonClick();
+    initFavListIfExist();
+
+}
+
+const initFavListIfExist = () => {
+    if (window.sessionStorage.getItem("Fav")) {
+        var recupFavs = window.sessionStorage.getItem("Fav");
+        var favsList = JSON.parse(recupFavs);
+        addFavResult(favsList);
+    }
+
 }
 
 const removeButtonNewBook = () => {
@@ -50,8 +61,11 @@ const newBookButtonClick = () => {
 const searchButtonClick = () => {
     var searchButton = document.getElementById("search");
     searchButton.addEventListener('click', function(e) {
-        e.preventDefault(); 
+        e.preventDefault();
+        removeSearchResult(); 
         ApiCallback();
+        removeNewFormSearch();
+        addNewFormSearch();
     });
 }
 
@@ -62,7 +76,6 @@ const ApiCallback = () => {
     request.onreadystatechange = function() {
         if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
             var response = JSON.parse(this.responseText);
-            console.log(response);
             searchResult(response);
         }
     };
@@ -91,14 +104,30 @@ const searchResult = (response) => {
 
 }
 
+const addSearchBookToSessionStorage = (books) => {
+    if (window.sessionStorage.getItem("SearchBook")) {
+        console.log("SearchBook exsiste");
+        window.sessionStorage.removeItem("SearchBook");
+        var stringBooks = JSON.stringify(books);
+        console.log(stringBooks);
+        window.sessionStorage.setItem("SearchBook", stringBooks);
+    } else {
+        var stringBooks = JSON.stringify(books);
+        console.log(stringBooks);
+        window.sessionStorage.setItem("SearchBook", stringBooks);    
+    }
+}
+
 const addSearchResult = (books) => {
+    addSearchBookToSessionStorage(books);
     var resultPosition = document.querySelector("#myBooks > hr");
-    var resultTitle = document.createElement("h2");
-    resultTitle.setAttribute("class","bookResults");
-    resultTitle.innerHTML +=  'Résultats de recherche';
+    var resultTitle = document.createElement("div");
+    resultTitle.setAttribute("id","bookResults");
+    resultTitle.innerHTML =  "<h2>Résultats de recherche</h2>";
     resultPosition.after(resultTitle);
+    resultPosition = document.querySelector("#bookResults > h2");
     var section = document.createElement("section");
-    section.setAttribute("class","bookResults");
+    section.setAttribute("id","sectionResults");
     if (books.length > 0 ) {
         for (let book of books) {
             section.innerHTML += '<article class=sectionBook ><div class="titleAndBookmark"><p class="titleBooks">Titre du livre : ' + book.title + '</p><button id="' + book.id + '" class="bookmarkButton fas fa-bookmark"  </button></div><p class="idBooks">Id : ' + book.id + '</p><p class="authorBooks">Auteur : ' + book.author + '</p><p class="descriptionBooks">Description : ' + book.description + '</p><img class = "imageBook" src=' + book.image + '></article>';
@@ -106,41 +135,116 @@ const addSearchResult = (books) => {
     } else {
         section.innerHTML = '<p class="searchNull">Aucun livre n’a été trouvé</p>'
     }
-    resultTitle.after(section);
+    resultPosition.after(section);
     bookmarkButtonClick();
 
 }
 
-const addBookToFav = (id) => {
-    var ids = [];
-    if (sessionStorage.getItem("Fav")) {  
-        ids.push(sessionStorage.getItem("Fav"));
-        ids.push(id);
-        sessionStorage.setItem('Fav', ids);
-      } else {
-            ids.push(id);
-            sessionStorage.setItem('Fav', ids);
-      }
+const addFavResult = (favs) => {
+    removeFavResult();
+    var resultPosition = document.querySelector("#content > h2");
+    var section = document.createElement("section");
+    section.setAttribute("id","sectionFav");
+    if (favs.length > 0 ) {
+        for (let fav of favs) {
+            section.innerHTML += '<article class=sectionBook ><div class="titleAndBookmark"><p class="titleBooks">Titre du livre : ' + fav.title + '</p><button id="' + fav.id + '" class="trashButton fas fa-trash-alt" </button></div><p class="idBooks">Id : ' + fav.id + '</p><p class="authorBooks">Auteur : ' + fav.author + '</p><p class="descriptionBooks">Description : ' + fav.description + '</p><img class = "imageBook" src=' + fav.image + '></article>';
+        }
+    } else {
+        section.innerHTML = '<p class="searchNull">Aucun livre n’a été trouvé</p>'
+    }
+    resultPosition.after(section);
+    trashButtonClick();
+
+}
+
+const addBookToSessionStorage = (id) => {
+    var recupSearchBook = window.sessionStorage.getItem("SearchBook");
+    var searchBookList = JSON.parse(recupSearchBook);
+    for (let book of searchBookList) {
+        if (book.id == id) {
+            var bookToAdd = book;
+        } 
+    }
+    var exist = false;
+    if (window.sessionStorage.getItem("Fav")) {
+        console.log("fav exsiste");
+        var recupFavs = window.sessionStorage.getItem("Fav");
+        var favsList = JSON.parse(recupFavs);
+
+        for (let fav of favsList ) {
+            if(bookToAdd.id == fav.id && exist == false) {
+                console.log("elert");
+                alert("déja dans les favoris");
+                exist = true;
+            }
+            
+        }
+        if (exist == false) {
+            favsList.push(bookToAdd);
+            var myFavs = JSON.stringify(favsList);
+            window.sessionStorage.setItem('Fav', myFavs);
+            
+
+        }
+    } else {
+            var favsList = [];
+            favsList.push(bookToAdd);
+            var myFavs = JSON.stringify(favsList);
+            window.sessionStorage.setItem('Fav', myFavs);      
+    }
+    console.log(favsList);
+    addFavResult(favsList);
+}
+
+const removeBookToSessionStorage = (id) => {
+    var recupFavs = window.sessionStorage.getItem("Fav");
+    var favsList = JSON.parse(recupFavs);
+        for (let favNum in favsList ) {
+            if(id == favsList[favNum].id ) {
+                console.log("suppression de : " + favsList[favNum].title);
+                if (favsList.length > 1) {
+                    var newFavsList = favsList.splice(favNum, 1);
+                    console.log("newFavsList : " + newFavsList);
+                    var myFavs = JSON.stringify(newFavsList);
+                    window.sessionStorage.setItem('Fav', myFavs);  
+                } else {
+                    window.sessionStorage.removeItem("Fav");
+                    favsList= [] ;
+                }
+            }
+        }
+        addFavResult(favsList);
 }
 
 const bookmarkButtonClick = () => {
     var bookmarkButtons = document.getElementsByClassName("bookmarkButton");
     for (let bookmarkButton of bookmarkButtons) {
         bookmarkButton.addEventListener('click', function(e) {
-            addBookToFav(e.target.id);              
+            addBookToSessionStorage(e.target.id);              
         });
     }
 }
 
+const trashButtonClick = () => {
+    var trashButtons = document.getElementsByClassName("trashButton");
+    for (let trashButton of trashButtons) {
+        trashButton.addEventListener('click', function(e) {
+            removeBookToSessionStorage(e.target.id);              
+        });
+    }
+}
 
 const removeSearchResult = () => {
-    var bookResults = document.getElementsByClassName("bookResults");
+    var bookResults = document.getElementById("bookResults");
     if (bookResults != null) {
-        for (let bookResult of bookResults ) {
-            console.log(bookResult);
-            bookResult.remove();
-        }
+            console.log(bookResults);
+            bookResults.remove();
     }
+}
+
+const removeFavResult = () => {
+    var favResults = document.getElementById("content");
+    favResults.innerHTML = "<h2>Ma poch'liste</h2>"
 }
 
 class Book  {
@@ -154,12 +258,3 @@ class Book  {
 }
 
 addButtonNewBook();
-
-
-//window.localStorage.getItem('item', 123)
-
-// author : .items[].volumeInfo.authors[0]
-// title : .items[].volumeInfo.title
-// id :  .items[].accessInfo.id
-//image : items[].volumeInfo.imageLinks.thumbmail
-// description : items[].volumeInfo.description   (200 caractère max)
